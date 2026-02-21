a4ead56d9f912cc1a04775b3f111ba8c8f07a4b5
Gabriel Vieira <ropke2020@gmail.com>
2026-02-21 16:26:22 -0300

    liquid glass

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hi_core/app/hi_mobile.dart';
 import 'package:hi_core/hi_core.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   // ENV continua sendo do app
   await dotenv.load(fileName: ".env");
 
